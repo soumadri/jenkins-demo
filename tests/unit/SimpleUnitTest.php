7927e5ea7230a4e358d41417336ba597cf0d7fd5
soumadri <you@example.com>
2018-06-27 14:00:12 +0530

    Added more unit tests

--- tests/unit/SimpleUnitTest.php
+++ tests/unit/SimpleUnitTest.php
@@ -24,4 +24,28 @@ class SimpleUnitTest extends \Codeception\Test\Unit
 
         $this->assertEquals($service->sayHello("Soumadri"), "Hello Soumadri");
     }
+
+    public function testSayHelloEmpty()
+    {
+        //Test the service is returning message properly
+        $service = new HelloService();
+
+        $this->assertEquals($service->sayHello(""), "Hello ");        
+    }
+
+    public function testSayHelloEmptyNegative()
+    {
+        //Test the service is returning message properly
+        $service = new HelloService();
+        
+        $this->assertNotEquals($service->sayHello(""), "Hello");
+    }
+
+    public function testSayHelloInUnicode()
+    {
+        //Test the service is returning message properly
+        $service = new HelloService();
+
+        $this->assertEquals($service->sayHello("வணக்கம்"), "Hello வணக்கம்");
+    }
 }
